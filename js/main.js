1e85de5c60bc2c61dfdf998acfb9c5df1e6a0ac8
itzhake <itzikil@icloud.com>
2022-10-12 14:50:45 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,77 @@
+'use strict'
+
+$(onInit)
+$('.submit-btn').click(sendEmail)
+
+function onInit() {
+    var projects = getProj()
+    renderPortfolio(projects)
+    renderPopup(projects)
+}
+
+function renderPortfolio(projects) {
+    var strHTML = projects.map(proj =>
+        `   <div class="col-md-4 col-sm-6 portfolio-item">
+          <a class="portfolio-link" data-toggle="modal" href="#${proj.id}">
+            <div class="portfolio-hover">
+              <div class="portfolio-hover-content">
+                <i class="fa fa-plus fa-3x"></i>
+              </div>
+            </div>
+            <img class="img-fluid" src="img/new portfolio/${proj.id}.png" alt="">
+          </a>
+          <div class="portfolio-caption">
+            <h4>${proj.name}</h4>
+            <p class="text-muted">${proj.title}</p>
+          </div>
+        </div>`)
+    $("#portfolio-container").html(strHTML)
+
+}
+
+function renderPopup(projects) {
+    var strHTML = projects.map(proj => `
+        <div class="portfolio-modal modal fade" id="${proj.id}" tabindex="-1" role="dialog" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="close-modal" data-dismiss="modal">
+                        <div class="lr">
+                            <div class="rl"></div>
+                        </div>
+                    </div>
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-lg-8 mx-auto">
+                                <div class="modal-body">
+                                    <!-- Project Details Go Here -->
+                                    <h2>${proj.name}</h2>
+                                    <p class="item-intro text-muted">${proj.title}</p>
+                                    <img class="img-fluid d-block mx-auto" src="img/new portfolio/${proj.id}.png" alt="">
+                                        <p>${proj.desc}</p>
+                                        <ul class="list-inline">
+                                            <li><a href="${proj.url}" target="_blank"><button class="btn">Check it Out</button></li></a>
+                                            <li>Date: January 2017</li>
+                                            <li>Client: Threads</li>
+                                            <li>Category: Illustration</li>
+                                        </ul>
+                                        <button class="btn btn-primary" data-dismiss="modal" type="button">
+                                            <i class="fa fa-times"></i>
+                                            Close Project</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>`
+    )
+    $("#modal-container").html(strHTML)
+}
+
+function sendEmail() {
+    var emailAdress = $('#email-adress').val().trim()
+    var subject = $('#subject').val().trim()
+    var message = $('#message').val().trim()
+    open('https://mail.google.com/mail/?view=cm&fs=1&to='+emailAdress+'&su='+subject+'&body = '+message)
+}
+// if (!emailAdress || !subject) return
---
+++ js/services/gallery-service.js
@@ -0,0 +1,35 @@
+'use strict'
+
+var gProjs = [
+    {
+        id: "pacman",
+        name: "Pacman",
+        title: "eat those coockies",
+        desc: "The nice old pacman game we all love",
+        url: "https://itzikil.github.io/pacman/",
+        publishedAt: 1448693940000,
+        labels: ["Matrixes", "keyboard events"],
+    },
+    {
+        id: "minesweeper",
+        name: "Minesweeper",
+        title: "beware the mines!",
+        desc: "The nice old minesweeper game we all love",
+        url: " https://itzikil.github.io/MineSweeper/",
+        publishedAt: 1448693940000,
+        labels: ["Matrixes", "keyboard events"],
+    },
+    {
+        id: "books-shop",
+        name: "Books Shop",
+        title: "New book shop!",
+        desc: "We have all the books you need",
+        url: "projs/sokoban",
+        publishedAt: 1448693940000,
+        labels: ["Matrixes", "keyboard events"],
+    },
+]
+
+function getProj(){
+    return gProjs
+}
